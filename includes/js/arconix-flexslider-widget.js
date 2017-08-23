6c17d272bd73ea24dc76d99879177df6e38436b3
Bhagyashri Lokhande <lokhandebhagyashri1@gmail.com>
2017-08-23 18:41:13 +0530

    fix #24

--- includes/js/arconix-flexslider-widget.js
+++ includes/js/arconix-flexslider-widget.js
@@ -9,6 +9,9 @@ jQuery( document ).ready( function() {
             if( jQuery( '.widget_post_type' )[ i ].value == 'post' ) {
                 jQuery( '.category' )[ i ].style.display = 'block';
                 jQuery( '.tag' )[ i ].style.display = 'block';
+            }else{
+            	jQuery( '.category' )[ i ].style.display = 'none';
+                jQuery( '.tag' )[ i ].style.display = 'none';
             }
         }
     } );
